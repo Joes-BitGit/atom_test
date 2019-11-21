57fb2980dba300f0470e716b86b6e84c126e8cbe
Joes-BitGit <josephnalmeida@gmail.com>
2019-11-20 16:05:32 -0800

    Ultra Commitment

---
+++ flaskblog/flaskblog.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return '<h2>Home Page!</h2>'
+
+@app.route('/about')
+def about():
+    return '<h2>About Page!</h2>'
+
+
+# to run using python3 flaskblog.py
+if __name__ == '__main__':
+    app.run(debug=True)
