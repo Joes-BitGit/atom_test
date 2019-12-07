40f057d77f6923fe0b7577b85d6867fc94941eda
Joes-BitGit <josephnalmeida@gmail.com>
2019-12-06 18:25:58 -0800

    Added registration and login form links

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,33 @@
 from flask_wtf from FlaskForm
-from wtforms from StringField
-
+from wtforms from StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
+    # checks
+    # 1. make sure something is inputted
+    # 2. make sure it is valid
+    username = StringField('Username',
+                            validators=[DataRequired(), Length(min=1, max=20)])
 
-    username = StringField('Username')
+    email = StringField('Email',validators=[DataRequired(), Email()])
+
+    password = StringField('Password',validators=[DataRequired()])
+
+    confirm_password = StringField('Confirm Password',
+                                    validators=[DataRequired(),
+                                                EqualTo('password')])
+
+    submit = SubmitField('Sign Up')
+
+class LogInForm(FlaskForm):
+    # checks
+    # 1. make sure something is inputted
+    # 2. make sure it is valid
+
+    email = StringField('Email',validators=[DataRequired(), Email()])
+
+    password = StringField('Password',validators=[DataRequired()])
+
+    remember = BooleanField('Remember Me')
+
+    submit = SubmitField('Login In')
